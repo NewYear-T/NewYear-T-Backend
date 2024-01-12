01bbc35168bcdf6aff56dc179ad4b61d7090d4b8
채승지 <cotmdwl1405@gmail.com>
2024-01-12 20:19:31 +0900

    chore : 변수 추가

--- src/main/java/com/example/newyear/Entity/Challenge.java
+++ src/main/java/com/example/newyear/Entity/Challenge.java
@@ -1,9 +1,8 @@
 package com.example.newyear.Entity;
 
-
-import com.example.newyear.Entity.enums.Local;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -16,13 +15,32 @@ import java.util.List;
 @AllArgsConstructor
 @Builder
 /**
- * 챌린지 정보
- */
+
+ 챌린지 정보*/
 public class Challenge {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String title; // 챌린지 제목
+
+    private String description; // 챌린지 설명
+
+    private LocalDateTime startTime; // 챌린지 시작 시간
+
+    private LocalDateTime endTime; // 챌린지 종료 시간
+
+    private int max_people; // 정원
+
+    @CreationTimestamp
+    private LocalDateTime createdAt; // 생성 시간
+
+    private LocalDateTime endAt; // 모집 마감 시간
+
+    @OneToMany(mappedBy = "challenge")
+    private List<Completed> completedList; // 완료 여부
+
     @ManyToMany(mappedBy = "challenges")
-    private List<Member> members;
+    private List<Member> members; // 멤버 정보
 }
